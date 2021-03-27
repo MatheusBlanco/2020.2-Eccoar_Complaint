52eee12b5afd55be339a25f850b9e24fcd7366e3
devsalula <saleh.nazih.dev@gmail.com>
2021-03-27 12:53:58 -0300

    Add Picture in Seed and fix description

--- src/entity/Complaint.ts
+++ src/entity/Complaint.ts
@@ -36,4 +36,7 @@ export class Complaint {
 
     @Column({nullable: false, default: false, type: "tinyint"})
     status: boolean;
+
+    @Column({length: 255, nullable: true})
+    picture: string;
 }
